6e1b19935b28ab77ad0e0d08f89f04e9c9b8de31
Przemyslaw Walczyk <pwalczykk@gmail.com>
2016-07-01 17:32:52 +0200

    Working dynamometer, changed plugin location in .xacro

--- rover_simulation/src/plugins/joint_dynamometer/JointDynamometer.cc
+++ rover_simulation/src/plugins/joint_dynamometer/JointDynamometer.cc
@@ -12,6 +12,9 @@ JointDynamometer::JointDynamometer() : ModelPlugin()
     this->msg.torque.x = 0;
     this->msg.torque.y = 0;
     this->msg.torque.z = 0;
+
+    this->period = 100;
+    this->counter = 0;
 }
 
 void JointDynamometer::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
@@ -34,17 +37,27 @@ void JointDynamometer::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
 
 void JointDynamometer::OnUpdate(const common::UpdateInfo &info)
 {
-
     this->wrench = joint->GetForceTorque(0);    //Index not used currently
-    this->msg.force.x = wrench.body1Force.x;
-    this->msg.force.y = wrench.body1Force.y;
-    this->msg.force.z = wrench.body1Force.z;
+    this->msg.force.x += wrench.body1Force.x;
+    this->msg.force.y += wrench.body1Force.y;
+    this->msg.force.z += wrench.body1Force.z;
 
-    this->msg.torque.x = wrench.body1Torque.x;
-    this->msg.torque.y = wrench.body1Torque.y;
-    this->msg.torque.z = wrench.body1Torque.z;
+    this->msg.torque.x += wrench.body1Torque.x;
+    this->msg.torque.y += wrench.body1Torque.y;
+    this->msg.torque.z += wrench.body1Torque.z;
 
-    this->pub.publish(msg);
+    counter++; if(counter > this->period) { counter = 0;
+
+        this->msg.force.x /= this->period;
+        this->msg.force.y /= this->period;
+        this->msg.force.z /= this->period;
+
+        this->msg.torque.x /= this->period;
+        this->msg.torque.y /= this->period;
+        this->msg.torque.z /= this->period;
+
+        this->pub.publish(msg);
+    }
 }
 
 ///////////////////////////// SYSTEM FUNCTIONS
@@ -54,8 +67,25 @@ void JointDynamometer::Load_Publisher(std::string topic, ros::Publisher& pub)
     pub = this->rosNode->advertise<geometry_msgs::Wrench>(topic, 100);
 }
 
+// void JointDynamometer::Load_Subscrieber(std::string topic,
+//                                         ros::Subscriber& sub,
+//                                         void (JointDynamometer::*function)(const std_msgs::Float64ConstPtr&))
+// {
+//     ros::SubscribeOptions so =
+//       ros::SubscribeOptions::create<std_msgs::Float64>(
+//           topic,
+//           1,
+//           boost::bind(function, this, _1),
+//           ros::VoidPtr(),
+//           &this->rosQueue);
+//
+//     sub = this->rosNode->subscribe(so);
+// }
+
 void JointDynamometer::Load_SimLoop()
 {
+    ROS_ERROR("5");
+
     this->updateConnection = event::Events::ConnectWorldUpdateBegin(
         boost::bind(&JointDynamometer::OnUpdate, this, _1));
     printf("SimLoop plugin launched succesfully\n");
@@ -63,6 +93,8 @@ void JointDynamometer::Load_SimLoop()
 
 void JointDynamometer::Load_NodeROS()
 {
+    ROS_ERROR("6");
+
     this->rosNode.reset(new ros::NodeHandle("gazebo_client"));
 
     this->rosQueueThread =
@@ -85,5 +117,4 @@ void JointDynamometer::QueueThread()
         this->rosQueue.callAvailable(ros::WallDuration(timeout));
     }
 }
-
 }
--- rover_simulation/src/plugins/joint_dynamometer/JointDynamometer.hh
+++ rover_simulation/src/plugins/joint_dynamometer/JointDynamometer.hh
@@ -23,6 +23,9 @@ namespace gazebo
         physics::JointPtr joint;
 
         ros::Publisher pub;
+        // ros::Subscriber sub;
+        int period;
+        int counter;
 
         geometry_msgs::Wrench msg;
         physics::JointWrench wrench;
@@ -30,14 +33,19 @@ namespace gazebo
     public:
         JointDynamometer();
 
-        void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf);
-        void Load_Publisher(std::string topic, ros::Publisher& pub);
-        void Load_SimLoop();
-        void Load_NodeROS();
-
         void OnUpdate(const common::UpdateInfo &info);
 
     private:
+        void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf);
+
+        void Load_Publisher(std::string topic, ros::Publisher& pub);
+        // void Load_Subscrieber(std::string topic,
+        //                       ros::Subscriber& sub,
+        //                       void (JointDynamometer::*function)(const std_msgs::Float64ConstPtr&));
+
+        void Load_SimLoop();
+        void Load_NodeROS();
+
         void QueueThread();
     };
     GZ_REGISTER_MODEL_PLUGIN(JointDynamometer)
