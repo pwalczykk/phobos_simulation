b5732811b23fb1945d842d7088ed5dbd5c9619a4
Przemek <pwalczykk@gmail.com>
2016-07-04 15:30:50 +0200

    joint_dynamometer: file opend and closed with each write model: added joint dynamometer to wheels and bogie bearing

--- rover_simulation/src/nodes/gazebo_joint_dynamometer_recorder/include/GazeboJointDynamometerRecorder.hpp
+++ rover_simulation/src/nodes/gazebo_joint_dynamometer_recorder/include/GazeboJointDynamometerRecorder.hpp
@@ -11,16 +11,18 @@ class GazeboJointDynamometerRecorder{
 
     std::fstream file;
 
+    std::string file_name;
+
 public:
     GazeboJointDynamometerRecorder(std::string file_name, std::string topic){
-        this->file.open(file_name, std::ios::app);
         this->sub = nh.subscribe(topic, 100, &GazeboJointDynamometerRecorder::UpdateWrench, this);
+        this->file_name = file_name;
     }
     ~GazeboJointDynamometerRecorder(){
-        this->file.close();
     };
 
     void UpdateWrench(const geometry_msgs::Wrench msg){
+        this->file.open(file_name, std::ios_base::out | std::ios_base::app);
         if(!this->file.is_open()){
             ROS_WARN("File not opened -> Not saving!");
             return;
@@ -28,6 +30,9 @@ public:
 
         file << msg.force.x << " " << msg.force.y << " " << msg.force.z << " "
         << msg.torque.x << " " << msg.torque.y << " " << msg.torque.z << std::endl;
+
+        this->file.close();
+
         return;
     }
 };
